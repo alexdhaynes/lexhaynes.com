76ecdec9bdf8ddca9045acc3679d94aedc60c3d8
Alex Haynes <lexhaynes@users.noreply.github.com>
2021-04-26 08:14:23 -0400

    footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 
+import navData from '@data/nav.json'
+import Link from 'next/link'
 
 const currentYear = new Date().getFullYear();
 
@@ -7,9 +9,22 @@ const Footer = () => {
         <footer className="footer p-20 mt-20 bg-yellow-100" id="skills">
             <div className="container">
                 <div className="flex align-center justify-center">
-                <p>Copyright &copy; {currentYear} Alexandra Haynes</p>
+                    <p>Copyright &copy; {currentYear} Alexandra Haynes</p>
                 </div>
+                <div className="mx-auto flex justify-between w-2/3 mt-6">
+                {
+                    navData.map((item, i) => (
+                        <Link key={`footerNav-${i}`}  href={item.href} title={item.displayName}>
+                            <a className="underline hover:text-gray-500 text-semibold mr-1">
+                                {item.displayName}
+                            </a>
+                        </Link>
+                    ))
+                }
+                </div>
+                
             </div>
+           
       </footer>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,7 +18,7 @@ const MenuDropDown = ({links}) => {
             <Link key={`navbar-link-${i}`} 
               href={nav.href} 
               >
-              <a className="w-full rounded-t-sm px-5 py-3 hover:bg-gray-700 hover:text-white">{nav.displayName}</a>
+              <a className="w-full rounded-t-sm px-5 py-3 hover:bg-gray-700 transition duration-100 hover:text-white">{nav.displayName}</a>
             </Link>
           ))
         }
