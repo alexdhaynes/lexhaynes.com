2b8ae069b09a016d72d6adb858118a36eeff779d
Alex Haynes <lexhaynes@users.noreply.github.com>
2021-04-26 08:34:00 -0400

    footer link styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,16 @@ const currentYear = new Date().getFullYear();
 
 const Footer = () => {
     return (
-        <footer className="footer p-20 mt-20 bg-yellow-100" id="skills">
-            <div className="container">
+        <footer className="footer py-6 bg-yellow-100" id="skills">
+            <div className="container w-11/12 mx-auto mb-10">
                 <div className="flex align-center justify-center">
                     <p>Copyright &copy; {currentYear} Alexandra Haynes</p>
                 </div>
-                <div className="mx-auto flex justify-between w-2/3 mt-6">
+                <div className="mx-auto flex flex-wrap w-full justify-center md:justify-between md:w-2/3 mt-6">
                 {
                     navData.map((item, i) => (
                         <Link key={`footerNav-${i}`}  href={item.href} title={item.displayName}>
-                            <a className="underline hover:text-gray-500 text-semibold mr-1">
+                            <a className="underline flex-none hover:text-gray-500 text-semibold mr-2 md:mr-0">
                                 {item.displayName}
                             </a>
                         </Link>
