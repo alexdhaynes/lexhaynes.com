5de8ce0301e2fe765f1508f49c9a5cb1a3302c1f
Alex Haynes <lexhaynes@users.noreply.github.com>
2021-04-26 16:51:58 -0400

    explicitly setting status as OK. lets see if that stops the timeout issue

--- src/pages/api/sendEmail.js
+++ src/pages/api/sendEmail.js
@@ -28,7 +28,7 @@ export default function handler(req, res) {
         // send email
         try {
             sendEmail(firstName, lastName, email, subject, message);
-            //res.status(200).json({msg: "success"})
+            res.status(200).json({msg: "success"})
         } catch (err) {
            res.status(500).json(err)
         }
