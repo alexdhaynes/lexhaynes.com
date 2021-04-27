58496d6a813fe57990fb2d95eb4959d927a3facf
Alex Haynes <lexhaynes@users.noreply.github.com>
2021-04-27 19:20:56 -0400

    added finally to try and prevent repeat requests

--- src/pages/api/sendEmail.js
+++ src/pages/api/sendEmail.js
@@ -32,6 +32,8 @@ export default function handler(req, res) {
             res.status(200).json({msg: "success"})
         } catch (err) {
            res.status(500).json(err)
+        } finally {
+            res.status(202).json({msg: "finally"})
         }
     } 
 }
