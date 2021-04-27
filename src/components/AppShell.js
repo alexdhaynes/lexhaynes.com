8ef986b495a840ad9f4779a8454e2e94fec54c50
Alex Haynes <lexhaynes@users.noreply.github.com>
2021-04-26 21:54:23 -0400

    fonts

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -13,7 +13,7 @@ const AppShell = ({title, children}) => {
                 <title>{title}</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700;900&family=Sen:wght@400;700&display=swap" rel="stylesheet" />        
+                <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Sen:wght@400&display=swap" rel="stylesheet" />        
             </Head>
 
             <NavBar />
